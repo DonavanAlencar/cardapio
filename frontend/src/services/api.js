4fb19d2898362179474d0c5d419bbdb8e4a58a67
DonavanAlencar <donavan.alencar@gmail.com>
2025-04-29 18:53:00 +0200

    Atribuição de variável de ambiente da api do backend para execução no kubernetes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  //baseURL: 'http://localhost:4000/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL,  // usa a env var
 });
 
 export default api;
